e6eaf0f83f13d8c35952b4ae4b9c8bcf9f1f3690
Ben Powell <powe0186@gmail.com>
2022-03-22 22:03:01 -0500

    added arrays to store character options. Created a function that will prompt a Y/N question

--- script.js
+++ script.js
@@ -2,8 +2,24 @@
 var generateBtn = document.querySelector("#generate");
 
 // Create arrays to hold capitals, lowercase, digits, and special characters.
+const capitals = ['A', 'B', 'C', 'D','E', 'F', 'G', 'H','I', 'J', 'K', 'L','M', 'N', 'O', 'P','Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z'];
+const lowerCase = ['a', 'b', 'c', 'd','e', 'f', 'g', 'h','i', 'j', 'k', 'l','m', 'n', 'o', 'p','q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z'];
+const numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specials = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}','~'];
 
 
+//A function that prompts a Y/N question. Then returns true or false.
+function yesOrNo(question) {
+  var answer = prompt(question +  " Y/N").toUpperCase();
+  while (answer != 'Y' && answer != 'N') {
+    answer = prompt("You must chose Y or N").toUpperCase();
+  }
+  if (answer === 'Y') {
+    return true;
+  } else {
+    return false;
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,10 +28,20 @@ function writePassword() {
   // Prompt the user for a password length. If the length is outside the range 8-128, the user is prompted again. 
   // store this value in a variable. I may need to change the variable from a string to a number.
 
+  var length = prompt("How long do you want your password to be. Please enter a number between 8 and 128");
+
+  //Make sure the user input is between 8 and 128.
+
+  while (length < 8 || length > 128) {
+    length = prompt("You need to enter a password length between 8 and 128!");
+  }
   // Prompt the user to type any characters they want included in the password.
   // Should this be separate  y/n prompts for lowercase, uppercase, special, numeric????? I'll assume that for now.
   // Prompt for each of these cases and store a boolean value in variable for each one.
-
+  
+  
+  
+  
 
   // create a new array of possibilities by using .concat on the chosen arrays.
 
